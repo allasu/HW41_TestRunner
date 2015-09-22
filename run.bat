9787d382e4009026517ac15a11ee76f1f3a57a55
Alla Sushon <allasu777@gmail.com>
2015-09-21 19:16:32 -0700

    Initial commit - windows runner

---
+++ run.bat
@@ -0,0 +1,48 @@
+@ECHO OFF
+::===============================
+SET GITHUB_ACCOUNT=allasu
+SET WS_DIR=Workspace
+SET REPO_NAME=Windows_E2E_Validation
+SET APP_VERSION=1.1
+::SET MAIN_CLASS=core.Title_Validation_CSV
+::SET ARGS_01=
+::================================
+IF "%JAVA_HOME%" == "" GOTO EXIT_JAVA
+ECHO Java installed
+IF "%M2%" == "" GOTO EXIT_MVN
+ECHO Maven installed
+CALL git --version > nul 2>&1
+IF NOT %ERRORLEVEL% == 0 GOTO EXIT_GIT
+ECHO Git installed
+
+GOTO NEXT
+
+:NEXT
+IF NOT EXIST C:\%WS_DIR% GOTO NO_WORKSPACE
+IF EXIST C:\%WS_DIR%\%REPO_NAME% RD /S /Q C:\%WS_DIR%\%REPO_NAME%
+ECHO Subdirectory %REPO_NAME% is removed
+CD C:\%WS_DIR%
+git clone https://github.com/%GITHUB_ACCOUNT%/%REPO_NAME%.git
+SLEEP 2
+CD %REPO_NAME%
+SLEEP 2
+CALL mvn clean site test -Dtest=AllTests -Dbuild.version="%APP_VERSION%"
+ECHO.
+SLEEP 2
+
+GOTO END
+
+:EXIT_JAVA
+ECHO No Java installed
+GOTO END
+:EXIT_MVN
+ECHO No Maven installed
+GOTO END
+:EXIT_GIT
+ECHO No Git installed
+GOTO END
+:NO_WORKSPACE
+ECHO %WS_DIR% does not exist
+GOTO END
+:END
+CD\
